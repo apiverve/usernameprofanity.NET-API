24084faf8e3d468343e3b37cd8dc9606db65cf7a
APIVerve Team <hello@apiverve.com>
2024-05-14 15:44:56 -0500

    Bug fixes and Improvements

---
+++ usernameprofanityQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class usernameprofanityQueryOptions {
+public string username { get; set; }
+}
+
+}
